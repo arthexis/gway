de2172eefbd6066e84edbaab1b6009bfe3a48ee8
R. J. Guillén-Osorio <arthexis@gmail.com>
2025-04-30 12:12:05 -0600

    autoinstall on first run

--- gway.bat
+++ gway.bat
@@ -4,6 +4,17 @@ setlocal
 :: Change to the directory containing this script
 cd /d "%~dp0"
 
+:: If .venv doesn't exist, create it and install gway in editable mode
+if not exist ".venv" (
+    echo Creating virtual environment...
+    python -m venv .venv
+    call .venv\Scripts\activate.bat
+    echo Installing gway in editable mode...
+    python -m pip install --upgrade pip
+    python -m pip install -e .
+    deactivate
+)
+
 :: Activate the virtual environment
 call .venv\Scripts\activate.bat
 
--- gway.sh
+++ gway.sh
@@ -4,6 +4,17 @@ set -e
 # Change to the directory where this script is located
 cd "$(dirname "$0")"
 
+# If .venv doesn't exist, create it and install gway in editable mode
+if [ ! -d ".venv" ]; then
+  echo "Creating virtual environment..."
+  python3 -m venv .venv
+  source .venv/bin/activate
+  echo "Installing gway in editable mode..."
+  pip install --upgrade pip
+  pip install -e .
+  deactivate
+fi
+
 # Activate the virtual environment
 source .venv/bin/activate
 
