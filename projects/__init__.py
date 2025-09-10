c788df8a93405f0d4750c50a69f4e7dcf6986c87
Rafael Jesús Guillén Osorio <arthexis@gmail.com>
2025-09-10 16:27:42 -0600

    Package projects for installation

---
+++ projects/__init__.py
@@ -0,0 +1,5 @@
+"""Project modules shipped with gway.
+
+This file allows the ``projects`` directory to be installed as a Python package
+so bundled utilities remain accessible after installation.
+"""
