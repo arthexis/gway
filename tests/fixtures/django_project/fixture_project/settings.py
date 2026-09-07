ab06bb9f386b71b46aabb90cc966181eeae2b269
arthexis <arthexis@gmail.com>
2026-09-07 08:28:46 -0600

    Remove colliding Django fixture settings

--- tests/fixtures/django_project/fixture_project/settings.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = "gway-django-fixture"
-DEBUG = False
-INSTALLED_APPS = ["django.contrib.contenttypes"]
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
-USE_TZ = True
-DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
