a31f95f269b6c6fe88b277fdda92d6514aa6303e
arthexis <arthexis@gmail.com>
2026-09-07 18:00:20 -0600

    Enable fixture management app

--- tests/fixtures/django_project/gway_django_fixture/settings.py
+++ tests/fixtures/django_project/gway_django_fixture/settings.py
@@ -1,6 +1,9 @@
 SECRET_KEY = "gway-django-fixture"
 DEBUG = False
-INSTALLED_APPS = ["django.contrib.contenttypes"]
+INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "gway_django_fixture.testapp",
+]
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
 USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
