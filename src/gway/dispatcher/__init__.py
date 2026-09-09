6cf480a22b4a630d450eb82d63facca2ed5fa0f8
arthexis <arthexis@gmail.com>
2026-09-09 10:14:07 -0600

    Mark dispatcher compatibility re-exports

--- src/gway/dispatcher/__init__.py
+++ src/gway/dispatcher/__init__.py
@@ -1,6 +1,17 @@
-from ..sigils import capture_cli_values, resolve_captured_cli_values
-from .arguments import _decode_structured_argv, _provided_positional_count
-from .dispatch import CommandNotFound, Dispatcher, DispatchError, _strict_fallback_missing
-from .prompt import _fill_required_options
+from ..sigils import (
+    capture_cli_values as capture_cli_values,
+    resolve_captured_cli_values as resolve_captured_cli_values,
+)
+from .arguments import (
+    _decode_structured_argv as _decode_structured_argv,
+    _provided_positional_count as _provided_positional_count,
+)
+from .dispatch import (
+    CommandNotFound,
+    Dispatcher,
+    DispatchError,
+    _strict_fallback_missing as _strict_fallback_missing,
+)
+from .prompt import _fill_required_options as _fill_required_options
 
 __all__ = ["CommandNotFound", "DispatchError", "Dispatcher"]
