2ce8c6930e83dfbe3d617b45e97e1bb80e579a15
arthexis <arthexis@gmail.com>
2026-09-09 10:11:32 -0600

    Preserve dispatcher sigil helper monkeypatch surface

--- src/gway/dispatcher/__init__.py
+++ src/gway/dispatcher/__init__.py
@@ -1,3 +1,4 @@
+from ..sigils import capture_cli_values, resolve_captured_cli_values
 from .arguments import _decode_structured_argv, _provided_positional_count
 from .dispatch import CommandNotFound, Dispatcher, DispatchError, _strict_fallback_missing
 from .prompt import _fill_required_options
