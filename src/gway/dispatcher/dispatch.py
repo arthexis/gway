87aa3abf01373a7ef430a8fbdfaa598c95311103
arthexis <arthexis@gmail.com>
2026-09-10 23:52:20 -0600

    Fix alias argument decoding order

--- src/gway/dispatcher/dispatch.py
+++ src/gway/dispatcher/dispatch.py
@@ -136,7 +136,7 @@ class Dispatcher:
                 raise
             command, argv = self._resolve_default_command(commands, project.default_command, tokens)
         alias_arguments = (project.alias_arguments or {}).get(project_name, ())
-        argv = [*alias_arguments, *decode_stage_escapes(argv)]
+        argv = list(decode_stage_escapes((*alias_arguments, *argv)))
         if interactive:
             argv = _fill_required_options(command, argv)
         argv = _decode_structured_argv(command, argv)
