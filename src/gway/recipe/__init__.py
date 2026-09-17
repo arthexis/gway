200ba87656cf7e2500aab6bdf98a7985bdd6aa2f
arthexis <arthexis@gmail.com>
2026-09-17 13:37:22 -0600

    Remove private recipe compatibility exports

--- src/gway/recipe/__init__.py
+++ src/gway/recipe/__init__.py
@@ -1,23 +1,9 @@
 from __future__ import annotations
 
 from .context import RecipeContext, child_recipe_context
-from .execution import (
-    RecipeSession,
-    _run_recipe_body as _run_recipe_body,
-    _run_recipe_from as _run_recipe_from,
-    run_recipe,
-)
-from .fitness import (
-    _evaluate_fitness_once as _evaluate_fitness_once,
-    _fitness_context as _fitness_context,
-    evaluate_fitness,
-)
+from .execution import RecipeSession, run_recipe
 from .model import RecipeError, RecipeStatement
-from .parser import (
-    _recipe_statement_lines as _recipe_statement_lines,
-    _recipe_statement_lines_from_source as _recipe_statement_lines_from_source,
-    recipe_statements,
-)
+from .parser import recipe_statements
 
 __all__ = [
     "RecipeContext",
