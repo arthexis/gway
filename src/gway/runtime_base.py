c162c832f4536cfd707666b7bb0dea47d6b47eac
arthexis <arthexis@gmail.com>
2026-09-17 09:53:05 -0600

    Restore UpgradeError runtime compatibility export

--- src/gway/runtime_base.py
+++ src/gway/runtime_base.py
@@ -29,7 +29,7 @@ from .service import ServiceManager
 from .stage import Stage
 from .store import run_store
 from .transfer import encode_transfer
-from .upgrade import UpgradeResult, Upgrader
+from .upgrade import UpgradeError, UpgradeResult, Upgrader
 
 _SELECTOR = re.compile(r"\[(?P<index>[1-9]\d*)\]\Z")
 _WILDCARD = "[*]"
