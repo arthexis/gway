2a51e192e48ddabc315e1fe8e42fa7febd80a8b1
arthexis <arthexis@gmail.com>
2026-09-19 13:13:26 -0600

    feat: expose Sous Chef scheduler API

--- gway/souschef/__init__.py
+++ gway/souschef/__init__.py
@@ -2,10 +2,13 @@
 
 from .manifest import duration, job_from_data, jobs_from_data, load
 from .model import DEFAULT_TIMEOUT, Job
+from .scheduler import RunResult, Scheduler
 
 __all__ = [
     "DEFAULT_TIMEOUT",
     "Job",
+    "RunResult",
+    "Scheduler",
     "duration",
     "job_from_data",
     "jobs_from_data",
