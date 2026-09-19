bbdff2ccc31d28057c302b5e70b615b7a662d1c2
arthexis <arthexis@gmail.com>
2026-09-19 13:42:46 -0600

    feat: expose bounded Sous Chef execution API

--- gway/souschef/__init__.py
+++ gway/souschef/__init__.py
@@ -1,5 +1,6 @@
 """Built-in Sous Chef recipe scheduler declarations."""
 
+from .executor import RecipeExecutionError, RecipeExecutor, RecipeFailure, RecipeTimeoutError
 from .manifest import duration, job_from_data, jobs_from_data, load
 from .model import DEFAULT_TIMEOUT, Job
 from .scheduler import RunResult, Scheduler
@@ -7,6 +8,10 @@ from .scheduler import RunResult, Scheduler
 __all__ = [
     "DEFAULT_TIMEOUT",
     "Job",
+    "RecipeExecutionError",
+    "RecipeExecutor",
+    "RecipeFailure",
+    "RecipeTimeoutError",
     "RunResult",
     "Scheduler",
     "duration",
