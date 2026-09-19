4c792333a9720ec9724de483f954810403b496c2
arthexis <arthexis@gmail.com>
2026-09-19 12:37:16 -0600

    feat: expose service lifecycle API

--- gway/service/__init__.py
+++ gway/service/__init__.py
@@ -1,10 +1,14 @@
 """Project-owned service declarations."""
 
+from .controller import Controller
 from .manifest import catalog_from_data, load, service_from_data
 from .model import Catalog, Service
+from .runtime import ProcessBackend
 
 __all__ = [
     "Catalog",
+    "Controller",
+    "ProcessBackend",
     "Service",
     "catalog_from_data",
     "load",
