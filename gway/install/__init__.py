4f5dd10fe3ac43adbc5a3ea834f028847cbba522
arthexis <arthexis@gmail.com>
2026-09-19 09:55:12 -0600

    Expose durable installation stashes

--- gway/install/__init__.py
+++ gway/install/__init__.py
@@ -3,6 +3,7 @@
 from .model import Installation, InstallRequest, UninstallRequest
 from .ops import install, uninstall
 from .paths import InstallPaths, data_root, install_paths
+from .stash import Stash
 from .state import InstallState
 
 __all__ = [
@@ -10,6 +11,7 @@ __all__ = [
     "InstallPaths",
     "InstallRequest",
     "InstallState",
+    "Stash",
     "UninstallRequest",
     "data_root",
     "install",
