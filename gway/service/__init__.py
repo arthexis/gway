4fa99b0fb83614c74a53b859715852318fcdceb4
arthexis <arthexis@gmail.com>
2026-09-19 18:03:47 -0600

    Drop obsolete service catalog export

--- gway/service/__init__.py
+++ gway/service/__init__.py
@@ -1,11 +1,10 @@
 """Service lifecycle policy for generic Gway launchables."""
 
 from .controller import Controller
-from .model import Catalog, Service
+from .model import Service
 from .runtime import ProcessBackend
 
 __all__ = [
-    "Catalog",
     "Controller",
     "ProcessBackend",
     "Service",
