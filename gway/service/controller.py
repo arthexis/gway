5537e1300aded0b36ab5f6a1470154f4b9402efe
arthexis <arthexis@gmail.com>
2026-09-19 18:48:02 -0600

    Use single-word service state module

--- gway/service/controller.py
+++ gway/service/controller.py
@@ -149,7 +149,7 @@ class Controller:
 
         from ..install.backends import get as get_backend
         from ..install.paths import install_paths
-        from ..install.service_state import ServiceInstallState
+        from ..install.servicestate import ServiceInstallState
 
         installation = getattr(self.gateway, "_installed", {}).get(
             definition.project
