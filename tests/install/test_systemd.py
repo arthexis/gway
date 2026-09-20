0a173be79084847099583e64ac48a7b48b59fa8a
arthexis <arthexis@gmail.com>
2026-09-19 18:48:08 -0600

    Use single-word service state module

--- tests/install/test_systemd.py
+++ tests/install/test_systemd.py
@@ -1,7 +1,7 @@
 import pytest
 
 from gway import Gateway
-from gway.install.service_state import ServiceInstallState
+from gway.install.servicestate import ServiceInstallState
 
 
 def test_service_install_systemd_materializes_unit(
