1dc6aba3f87ee64cad7de63f51d24518df18e7b6
arthexis <arthexis@gmail.com>
2026-09-22 14:01:18 -0600

    Restore O2 remote account linking on main

--- gway/remote/__init__.py
+++ gway/remote/__init__.py
@@ -1,11 +1,22 @@
 """Shared remote-access infrastructure for G-Way."""
 
+from .account import RemoteAccountApplication
 from .metadata import RemoteOAuthMetadata
-from .server import RemoteDiscoveryApplication, build_server, serve
+from .server import (
+    RemoteApplication,
+    RemoteDiscoveryApplication,
+    build_server,
+    serve,
+)
+from .session import RemoteSession, RemoteSessionStore
 
 __all__ = [
+    "RemoteAccountApplication",
+    "RemoteApplication",
     "RemoteDiscoveryApplication",
     "RemoteOAuthMetadata",
+    "RemoteSession",
+    "RemoteSessionStore",
     "build_server",
     "serve",
 ]
