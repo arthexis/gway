61192241fcfe449009450a53e0567ee2c296b0dd
arthexis <arthexis@gmail.com>
2026-09-23 12:47:13 -0600

    Remove obsolete transitional environment test

--- tests/architecture/test_semantic_environment.py
+++
@@ -1,5 +0,0 @@
-from gway.environment import TRANSITIONAL_SEMANTIC_ENVIRONMENT
-
-
-def test_transitional_semantic_environment_inventory_is_empty():
-    assert TRANSITIONAL_SEMANTIC_ENVIRONMENT == frozenset()
