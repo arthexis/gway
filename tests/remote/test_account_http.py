49d96e422510e07c1960c143b205c03ab72054de
arthexis <arthexis@gmail.com>
2026-09-23 19:34:33 -0600

    CI: move browser HTTP acceptance tests to heavy suite

--- tests/remote/test_account_http.py
+++ tests/remote/test_account_http.py
@@ -3,12 +3,17 @@ import re
 import threading
 from urllib.parse import urlencode
 
+import pytest
+
 from gway.remote.account import RemoteAccountApplication
 from gway.remote.server import build_server
 from gway.remote.session import RemoteSessionStore
 from gway.security.oauth import OAuthRegistry
 from gway.security.scopes import ScopeRegistry
 from gway.security.tokens import TokenRegistry
+
+
+pytestmark = pytest.mark.heavy
 
 
 def _csrf(html):
