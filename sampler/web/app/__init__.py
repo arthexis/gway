8125b498ea94412cbc67b830cd7d072f4ca437df
arthexis <arthexis@gmail.com>
2026-09-25 16:58:49 -0600

    1109 expose sampler web app capability surface

--- sampler/web/app/__init__.py
+++ sampler/web/app/__init__.py
@@ -1,5 +1,26 @@
 """Optional web application sampler capability."""
 
+from .adapter import HandlerNotFound, InMemoryAdapter, MethodNotAllowed, RouteNotFound
 from .application import register
+from .appspec import AppSpec, BindingSpec, RouteSpec, ViewSpec
+from .exposure import ExposureSpec, LocalAppService
+from .server import ApplicationHTTPAdapter, ApplicationRequest, build_app_server, build_server, serve_app
 
-__all__ = ["register"]
+__all__ = [
+    "AppSpec",
+    "ApplicationHTTPAdapter",
+    "ApplicationRequest",
+    "BindingSpec",
+    "ExposureSpec",
+    "HandlerNotFound",
+    "InMemoryAdapter",
+    "LocalAppService",
+    "MethodNotAllowed",
+    "RouteNotFound",
+    "RouteSpec",
+    "ViewSpec",
+    "build_app_server",
+    "build_server",
+    "register",
+    "serve_app",
+]
