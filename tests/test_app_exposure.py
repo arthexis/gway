b544b9a1925685b7aa5c8a84b52fb5315f578cae
arthexis <arthexis@gmail.com>
2026-09-25 16:59:45 -0600

    1109 update tests for sampler-owned web app

--- tests/test_app_exposure.py
+++ tests/test_app_exposure.py
@@ -1,7 +1,11 @@
 import pytest
 
-from gway.appexposure import ExposureSpec, LocalAppService
-from gway.appspec import AppSpec
+from gway.sampler import load as load_sampler
+
+web_app = load_sampler("web/app")
+ExposureSpec = web_app.ExposureSpec
+LocalAppService = web_app.LocalAppService
+AppSpec = web_app.AppSpec
 
 
 def test_expose_app_requires_known_local_target(gateway):
